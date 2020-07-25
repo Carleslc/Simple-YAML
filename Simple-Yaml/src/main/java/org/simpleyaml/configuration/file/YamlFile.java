39e8097490c418c7bd911e5434f05c63426c4465
Hasan Demirtaş <utsukushihito@outlook.com>
2020-07-25 22:00:33 +0300

    Update YamlFile.java

--- Simple-Yaml/src/main/java/org/simpleyaml/configuration/file/YamlFile.java
+++ Simple-Yaml/src/main/java/org/simpleyaml/configuration/file/YamlFile.java
@@ -386,8 +386,11 @@ public class YamlFile extends YamlConfiguration implements Commentable {
      */
     @Override
     public String toString() {
+        if (this.commentMapper == null) {
+            return this.saveToString();
+        }
         try {
-            return this.commentMapper == null ? this.saveToString() : this.saveToStringWithComments();
+            return this.saveToStringWithComments();
         } catch (final IOException e) {
             return e.getMessage();
         }
