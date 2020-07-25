466972125eea4ef6f24df5dda65a574a6916b692
Hasan Demirtaş <utsukushihito@outlook.com>
2020-07-25 23:45:53 +0300

    Create YamlConfigurationOptionsTest.java

---
+++ Simple-Yaml/src/test/java/org/simpleyaml/configuration/file/YamlConfigurationOptionsTest.java
@@ -0,0 +1,31 @@
+package org.simpleyaml.configuration.file;
+
+import org.junit.jupiter.api.Test;
+
+class YamlConfigurationOptionsTest {
+
+    @Test
+    void configuration() {
+    }
+
+    @Test
+    void copyDefaults() {
+    }
+
+    @Test
+    void pathSeparator() {
+    }
+
+    @Test
+    void header() {
+    }
+
+    @Test
+    void copyHeader() {
+    }
+
+    @Test
+    void indent() {
+    }
+
+}
