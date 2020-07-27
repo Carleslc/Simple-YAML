2526dae1013a74dc8b19ca6fe3ca9b817cfcd493
Hasan Demirtaş <utsukushihito@outlook.com>
2020-07-27 15:06:54 +0300

    Update YamlFileTest.java

--- Simple-Yaml/src/test/java/org/simpleyaml/configuration/file/YamlFileTest.java
+++ Simple-Yaml/src/test/java/org/simpleyaml/configuration/file/YamlFileTest.java
@@ -66,8 +66,49 @@ class YamlFileTest {
     }
 
     @Test
-    void loadWithComments() {
-
+    void loadWithComments() throws Exception {
+        final YamlFile yamlFile = new YamlFile(YamlFileTest.getResourcePath("test-comments.yml"));
+        final String content = "#####################\n" +
+            "## INITIAL COMMENT ##\n" +
+            "#####################\n" +
+            '\n' +
+            "# Test comments\n" +
+            "test:\n" +
+            "  number: 5\n" +
+            "  # Hello!\n" +
+            "  string: Hello world\n" +
+            "  boolean: true\n" +
+            "  # List of words\n" +
+            "  list:\n" +
+            "  - Each\n" +
+            "  - word\n" +
+            "  - will\n" +
+            "  - be\n" +
+            "  - in\n" +
+            "  - a\n" +
+            "  - separated\n" +
+            "    # Comment on a list item\n" +
+            "  - entry # :)\n" +
+            '\n' +
+            "# Wonderful number\n" +
+            "math:\n" +
+            "  pi: 3.141592653589793\n" +
+            "  # Comment without direct key\n" +
+            '\n' +
+            "# Some timestamps\n" +
+            "timestamp:\n" +
+            "  # ISO\n" +
+            "  canonicalDate: 2020-07-04T13:18:04.458Z\n" +
+            "  # Date/Time with format\n" +
+            "  formattedDate: 04/07/2020 15:18:04\n" +
+            '\n' +
+            "# End\n";
+        yamlFile.loadWithComments();
+        MatcherAssert.assertThat(
+            "Couldn't load the file with comments!",
+            yamlFile.saveToStringWithComments(),
+            new IsEqual<>(content)
+        );
     }
 
     @Test
